303b9ab996c48651b1965dea6a573df7e49a116b
dim286754 <dim286754@gmail.com>
2021-04-01 16:57:26 +0500

    Commit 4

--- Check.py
+++ Check.py
@@ -1,22 +1,3 @@
-import re
-#Vseskobki=[]
-#Formula=input('Введите формулу: ')
-#Vseskobki=input('Введите скобки, которые хотите проверить: ')
-
-#skobki=list(Vseskobki)
-#print(skobki)
-#skobkiinformula=re.search(r"[(){}[\]]+",Formula)
-#print(skobkiinformula)
-
-#s=Formula
-#keep = {"(",")","[","]","{","}"}
-#print("".join([ch for ch in s if ch in keep]))
-#a="(){}[]<>"
-#if Vseskobki in a and Vseskobki in Formula:
-
-#else:
-#    print('Таких скобок нет')
-
 s=input()
 form=list(s)
 print(form)
