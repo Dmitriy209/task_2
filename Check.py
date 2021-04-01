d26ca69a0c486c8bd2f8204dc02d627d2273b3c9
dim286754 <dim286754@gmail.com>
2021-04-01 10:14:29 +0500

    Commit 2

--- Check.py
+++ Check.py
@@ -16,3 +16,33 @@ import re
 
 #else:
 #    print('Таких скобок нет')
+
+s=input()
+stack=[]
+skobki=input()
+if skobki in '({[<':
+    good=True
+    for i in s:
+        if i in '([{<':
+            stack.append(i)
+        elif i in ')]}>':
+            if not stack:
+                good=False
+                break
+            open_bracket=stack.pop()
+            if open_bracket in skobki and open_bracket=='(' and i==')':
+                continue
+            if open_bracket in skobki and open_bracket=='[' and i==']':
+                continue
+            if open_bracket in skobki and open_bracket=='{' and i=='}':
+                continue
+            if open_bracket in skobki and open_bracket=='<' and i=='>':
+                continue
+            good=False
+            break
+    if good and len(stack)==0:
+        print(good)
+    else:
+        print(good)
+else:
+    print('XER')
